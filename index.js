68b55112479c3bcc6c2230c1dffc2a8324889b2c
BrunoTruber <brunotruber45@gmail.com>
2021-09-06 20:18:13 -0300

    new

--- index.js
+++ index.js
@@ -36,20 +36,18 @@ const home = require("./components/home/home");
 
 	//CORS
 
-	app.all("/*", (req, res, next) => {
-		res.header(
-			"Access-Control-Allow-Origin", "*",
-			"http://backend-rick-morty-bruno.herokuapp.com"
-			);
+	// app.all("/*", (req, res, next) => {
+	// 	res.header(
+	// 		"Access-Control-Allow-Origin", "*");
 
-		res.header("Access-Control-Allow-Methods", "*");
+	// 	res.header("Access-Control-Allow-Methods", "*");
 
-		res.header(
-			"Access-Control-Allow-Headers",
-			"Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
-		);
-		next();
-	});
+	// 	res.header(
+	// 		"Access-Control-Allow-Headers",
+	// 		"Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
+	// 	);
+	// 	next();
+	// });
 	//[GET] - Home
 	app.get("/", async (req, res) => {
 		res.send({ info: "Olá, Blue" });
